8358c9d3433dcbf990a36f952c0d69f94cf7b7c9
Sean Murphy <sean@iamseanmurphy.com>
2026-09-16 16:51:38 +0000

    1.110

--- simpleanalytics/simple-analytics.php
+++ simpleanalytics/simple-analytics.php
@@ -1,14 +1,14 @@
 <?php
 /*
  * Plugin Name: Simple Analytics Official
- * Version: 1.109
+ * Version: 1.110
  * Plugin URI: https://docs.simpleanalytics.com/install-simple-analytics-on-wordpress
  * Description: Embed Simple Analytics script in your WordPress website
  * Author: Simple Analytics
  * Author URI: https://simpleanalytics.com/
- * Requires at least: 6.9.4
- * Requires PHP: 7.2.0
- * Tested up to: 7.0.2
+ * Requires at least: 7.0.3
+ * Requires PHP: 7.4.0
+ * Tested up to: 7.1
  *
  * Text Domain: simple-analytics
  * Domain Path: /lang/
@@ -48,6 +48,7 @@ require __DIR__ . '/src/Actions/AddInactiveComment.php';
 require __DIR__ . '/src/Actions/AddNoScriptTag.php';
 require __DIR__ . '/src/Actions/AddPluginSettingsLink.php';
 require __DIR__ . '/src/Support/Str.php';
+require __DIR__ . '/src/Support/OnloadCallback.php';
 require __DIR__ . '/src/Settings/Block.php';
 require __DIR__ . '/src/Settings/Blocks/CalloutBlock.php';
 require __DIR__ . '/src/Settings/Concerns/HasDocs.php';
@@ -122,7 +123,7 @@ $adminPage = SimpleAnalytics\Settings\AdminPage::title('Simple Analytics')
             ->description('Collect analytics from visitors with disabled or no JavaScript.');
 
         $tab->input(SettingName::ONLOAD_CALLBACK, 'Onload Callback')
-            ->description('JavaScript function to call when the script is loaded.')
+            ->description('JavaScript to run when the script loads. Requires permission to save unfiltered HTML. After upgrading, save an existing callback again to enable it.')
             ->placeholder('Example: sa_event("My event")')
             ->docs('https://docs.simpleanalytics.com/trigger-custom-page-views#use-custom-collection-anyway');
 
--- simpleanalytics/src/Scripts/AnalyticsScript.php
+++ simpleanalytics/src/Scripts/AnalyticsScript.php
@@ -7,6 +7,7 @@ use SimpleAnalytics\Scripts\Contracts\HideScriptId;
 use SimpleAnalytics\Scripts\Contracts\Script;
 use SimpleAnalytics\Setting;
 use SimpleAnalytics\SettingName;
+use SimpleAnalytics\Support\OnloadCallback;
 
 class AnalyticsScript implements Script, HasAttributes, HideScriptId
 {
@@ -27,8 +28,8 @@ class AnalyticsScript implements Script, HasAttributes, HideScriptId
             'data-mode'         => Setting::boolean(SettingName::HASH_MODE) ? 'hash' : null,
             'data-collect-dnt'  => Setting::boolean(SettingName::COLLECT_DNT) ? 'true' : null,
             'data-ignore-pages' => Setting::get(SettingName::IGNORE_PAGES),
-            'data-auto-collect' => Setting::get(SettingName::MANUAL_COLLECT) ? 'true' : null,
-            'data-onload'       => Setting::get(SettingName::ONLOAD_CALLBACK),
+            'data-auto-collect' => Setting::boolean(SettingName::MANUAL_COLLECT) ? 'false' : null,
+            'onload'            => OnloadCallback::get(),
             'data-sa-global'    => Setting::get(SettingName::SA_GLOBAL),
             'data-hostname'     => Setting::get(SettingName::HOSTNAME),
         ]);
